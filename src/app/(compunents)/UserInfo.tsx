78ce2f269661c29fd4290ab921c4c6fcf7867ea7
shahzaibdvlpr <dellrugged5414@gmail.com>
2024-04-06 10:04:35 +0500

    complete 2

--- src/app/(compunents)/UserInfo.tsx
+++ src/app/(compunents)/UserInfo.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 
 export default function UserInfo() {
   const { data: session } = useSession();
+  //hdfjyrjyjrjrjy
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-8 bg-zinc-300/10 flex flex-col gap-2 my-6">
